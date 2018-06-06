a6d766e467df2c598dd81fb821b980e70dea27ba
影浅 <seekwe@gmail.com>
2018-06-06 17:25:42 +0800

    fix:非中文导出

--- src/Excel.php
+++ src/Excel.php
@@ -2,11 +2,13 @@
 
 namespace Zls\Action;
 
+use Z;
+
 /**
  * 导出数据为excel
  * @author 影浅-Seekwe
  * @link   seekwe@gmail.com
- * @since  0.0.1
+ * @since  0.0.2
  */
 class Excel
 {
@@ -15,8 +17,12 @@ class Excel
         if (!$filename) {
             $filename = date('Y-m-d H:i:s');
         }
+        $filename = trim(strtolower($filename));
+        if (!Z::strEndsWith($filename, '.csv')) {
+            $filename = $filename . 'csv';
+        }
         header("Content-type:text/csv");
-        header("Content-Disposition:attachment;filename=" . $filename . '.csv');
+        header("Content-Disposition:attachment;filename=" . $filename);
         header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
         header('Expires:0');
         header('Pragma:public');
@@ -26,17 +32,16 @@ class Excel
     {
         if (!$fp) {
             $fp = fopen('php://output', 'a');
+            fprintf($fp, chr(0xEF) . chr(0xBB) . chr(0xBF));
         }
         if (!$head && !$data) {
             fclose($fp);
         } else {
             if ($head) {
-                if (\is_string($head)) {
-                    $head = \explode(',', $head);
+                if (is_string($head)) {
+                    $head = explode(',', $head);
                 }
-                foreach ($head as $i => $v) {
-                    $head[$i] = iconv('utf-8', 'gbk//IGNORE', $v);
-                }
+                //foreach ($head as $i => $v) {$head[$i] = iconv('utf-8', 'gbk//IGNORE', $v);}
                 fputcsv($fp, $head);
             }
             if ($data) {
@@ -51,12 +56,12 @@ class Excel
                         $cnt = 0;
                     }
                     $row = $data[$t];
-                    foreach ($row as $i => $v) {
-                        $row[$i] = @iconv('utf-8', 'gbk//IGNORE', $v);
-                        //也许有特殊字符编码呢
-                        //    $row[$i] = mb_convert_encoding($v, 'gbk', 'utf-8');
-                        //}
-                    }
+                    //foreach ($row as $i => $v) {
+                    //    $row[$i] = @iconv('utf-8', 'gbk//IGNORE', $v);
+                    //    //也许有特殊字符编码呢
+                    //    //    $row[$i] = mb_convert_encoding($v, 'gbk', 'utf-8');
+                    //    //}
+                    //}
                     fputcsv($fp, $row);
                     unset($row);
                 }
