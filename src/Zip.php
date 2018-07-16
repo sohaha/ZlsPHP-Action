4eac753c91280682228b0e7c23bf62ee0d4fa511
seekwe <seekwe@gmail.com>
2018-07-16 13:44:27 +0800

    Zip

--- src/Zip.php
+++ src/Zip.php
@@ -5,13 +5,17 @@
  * @link   seekwe@gmail.com
  * @since  0.0.1
  */
+
 namespace Zls\Action;
+
 use Z;
+
 class Zip
 {
     private $errorMsg;
     private $errorCode;
     private $saveFile;
+
     /**
      * 解压文件
      * @param      $filename
@@ -29,18 +33,21 @@ class Zip
             if ($del === true) {
                 z::rmdir($path, false);
             }
+            /*将文件名和路径转成windows系统默认的gb2312编码，否则将会读取不到*/
             $filename = iconv("utf-8", "gb2312", $filename);
             $path = iconv("utf-8", "gb2312", $path);
             $resource = zip_open($filename);
             while ($dir_resource = zip_read($resource)) {
                 if (zip_entry_open($resource, $dir_resource)) {
                     $file_name = $path . zip_entry_name($dir_resource);
+                    /*以最后一个“/”分割,再用字符串截取出路径部分*/
                     $file_path = substr($file_name, 0, strrpos($file_name, "/"));
                     if (!is_dir($file_path)) {
                         mkdir($file_path, 0777, true);
                     }
                     if (!is_dir($file_name)) {
                         $file_size = zip_entry_filesize($dir_resource);
+                        /*如果文件过大，跳过解压，继续下一个*/
                         if ($file_size < $maxSize) {
                             $file_content = zip_entry_read($dir_resource, $file_size);
                             if ($fn instanceof \Closure) {
@@ -58,12 +65,15 @@ class Zip
             $endtime = explode(' ', microtime());
             $thistime = $endtime[0] + $endtime[1] - ($starttime[0] + $starttime[1]);
             $thistime = round($thistime, 3);
+
             return ['thistime' => $thistime];
         } else {
             $this->setError(404, '文件 ' . $filename . ' 不存在');
         }
+
         return false;
     }
+
     /**
      * @param $code
      * @param $msg
@@ -73,6 +83,7 @@ class Zip
         $this->errorCode = $code;
         $this->errorMsg = $msg;
     }
+
     /**
      * 压缩文件
      * @param        $path
@@ -87,71 +98,61 @@ class Zip
      */
     public function zip($path, $save, $manage = null, $overwrite = false, $prefix = '', $include = [], $debug = false)
     {
-        try {
-            $zip = new \ZipArchive();
-            $files = [];
-            $this->saveFile = $save;
-            $zipState = $zip->open($save, $overwrite ? \ZIPARCHIVE::OVERWRITE : \ZIPARCHIVE::CREATE);
-            if ($zipState === true) {
-                $this->initFilesList($path, $files);
-                foreach ($files as $file) {
-                    $localname = str_replace($path, $prefix, $file);
-                    $localname = ltrim($localname, '/');
-                    if ($manage instanceof \Closure) {
-                        $manageRes = $manage($localname, $file);
-                        if ($manageRes === false) {
-                            continue;
-                        } elseif (is_string($manageRes)) {
-                            $zip->addFromString($localname, $manageRes);
-                            continue;
-                        }
-                    }
-                    $zip->addFile($file, $localname);
+        $files = [];
+        /** @var \ZipArchive $zip */
+        $zip = $this->getZip($save, $overwrite);
+        $this->initFilesList($path, $files);
+        foreach ($files as $file) {
+            $localname = str_replace($path, $prefix, $file);
+            $localname = ltrim($localname, '/');
+            if ($manage instanceof \Closure) {
+                $manageRes = $manage($localname, $file);
+                if ($manageRes === false) {
+                    continue;
+                } elseif (is_string($manageRes)) {
+                    $zip->addFromString($localname, $manageRes);
+                    continue;
                 }
-                if ($include) {
-                    foreach ($include as $v) {
-                        $localname = str_replace($path, $prefix, $v);
-                        $localname = ltrim($localname, '/');
-                        $zip->addFile(z::realPath('../' . $v), $localname);
-                    }
-                }
-                $res = $save;
-                if ($debug) {
-                    $debug = [];
-                    for ($i = 0; $i < $zip->numFiles; $i++) {
-                        $debug[] = $zip->getNameIndex($i);
-                    }
-                    $res = $debug;
-                }
-                $zip->close();
-                return $res;
-            } else {
-                throw new \Exception('open [ ' . $save . ' ] error, ' . $this->errorMessage($zipState));
             }
-        } catch (\Exception $exc) {
-            $this->setError(500, $exc->getMessage());
-            return false;
+            $zip->addFile($file, $localname);
         }
+        if ($include) {
+            foreach ($include as $v) {
+                $localname = str_replace($path, $prefix, $v);
+                $localname = ltrim($localname, '/');
+                $zip->addFile(z::realPath('../' . $v), $localname);
+            }
+        }
+        $res = $save;
+        if ($debug) {
+            $debug = [];
+            for ($i = 0; $i < $zip->numFiles; $i++) {
+                $debug[] = $zip->getNameIndex($i);
+            }
+            $res = $debug;
+        }
+        /*关闭处理的zip文件*/
+        $zip->close();
+
+        return $res;
     }
+
     /**
-     * 初始化文件列表
-     * @param             $path
-     * @param             $file
+     * @param $save
+     * @param $overwrite
+     * @return \ZipArchive
      */
-    private function initFilesList($path, &$file)
+    public function getZip($save, $overwrite)
     {
-        $handler = opendir($path);
-        while (($filename = readdir($handler)) !== false) {
-            if ($filename != "." && $filename != "..") {
-                if (is_dir($path . "/" . $filename)) {
-                    $this->initFilesList($path . "/" . $filename, $file);
-                } else {
-                    $file[] = $path . "/" . $filename;
-                }
-            }
-        }
-        closedir($handler);
+        /** @var \ZipArchive $zip */
+        $zip = new \ZipArchive();
+        $this->saveFile = $save;
+        $zipState = $zip->open($save, $overwrite ? \ZIPARCHIVE::OVERWRITE : \ZIPARCHIVE::CREATE);
+        z::throwIf($zipState !== true, 500, 'open [ ' . $save . ' ] error, ' . $this->errorMessage($zipState));
+
+        return $zip;
     }
+
     public function errorMessage($code)
     {
         switch ($code) {
@@ -207,6 +208,31 @@ class Zip
                 return 'An unknown error has occurred(' . intval($code) . ')';
         }
     }
+
+    /**
+     * 初始化文件列表
+     * @param             $path
+     * @param             $file
+     */
+    private function initFilesList($path, &$file)
+    {
+        /*打开当前文件夹由$path指定。*/
+        $handler = opendir($path);
+        while (($filename = readdir($handler)) !== false) {
+            /*文件夹文件名字为'.'和‘..’，不要对他们进行操作*/
+            if ($filename != "." && $filename != "..") {
+                /*如果读取的某个对象是文件夹，则递归*/
+                if (is_dir($path . "/" . $filename)) {
+                    $this->initFilesList($path . "/" . $filename, $file);
+                } else {
+                    /*将文件加入zip对象*/
+                    $file[] = $path . "/" . $filename;
+                }
+            }
+        }
+        closedir($handler);
+    }
+
     /**
      * 下载压缩包
      * @param string $filename
@@ -224,6 +250,7 @@ class Zip
         z::header('Content-Length: ' . filesize($filename));
         @readfile($filename);
     }
+
     /**
      * @return array
      */
